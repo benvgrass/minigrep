1bca05f66f8fdec29431c2a5418a90bb1cab27d6
Ben Grass <bvgrass@outlook.com>
2023-10-03 15:11:14 -0400

    read command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = args.get(1).expect("Search String Expected");
+    let file_path = args.get(2).expect("File Path Expected");
+    println!("Searching for {query} in file {file_path}");
 }
