578217ca60a8f187ec392e2530697e3a8ede5ea4
Ben Grass <bvgrass@outlook.com>
2023-10-03 16:34:48 -0400

    implemented case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     search_results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut search_results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            search_results.push(line);
+        }
+    }
+    search_results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +59,20 @@ Rust:
 safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+
     }
 }
