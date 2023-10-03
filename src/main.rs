ff8273092a7b319ac77599f9f8a553e4ca280096
Ben Grass <bvgrass@outlook.com>
2023-10-03 16:47:24 -0400

    print error messages to standard error

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use minigrep::QueryConfig;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = QueryConfig::build(&args).unwrap_or_else(|err| {
-        println!("Issue parsing arguments: {err}");
+        eprintln!("Issue parsing arguments: {err}");
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        println!("Error running application: {e}");
+        eprintln!("Error running application: {e}");
         process::exit(1);
     }
 
