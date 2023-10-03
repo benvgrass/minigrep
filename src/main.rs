603c2ba6e1ff59a935ca7205849a999630a37d95
Ben Grass <bvgrass@outlook.com>
2023-10-03 15:17:02 -0400

    read file contents

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::env;
-
+use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = args.get(1).expect("Search String Expected");
     let file_path = args.get(2).expect("File Path Expected");
     println!("Searching for {query} in file {file_path}");
+    let file_contents = fs::read_to_string(file_path).expect("Unable to read file.");
+    println!("File text:\n{file_contents}")
+    // TODO: check for errors
 }
