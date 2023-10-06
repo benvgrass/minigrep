af5d4fcee3745a5911e43525a2a0e9a0c1eb2e21
Ben Grass <bvgrass@outlook.com>
2023-10-05 21:17:09 -0400

    use iterators in search methods

--- src/lib.rs
+++ src/lib.rs
@@ -42,24 +42,12 @@ pub fn run(config: QueryConfig) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut search_results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            search_results.push(line);
-        }
-    }
-    search_results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut search_results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            search_results.push(line);
-        }
-    }
-    search_results
+    contents.lines().filter(|line| line.to_lowercase().contains(query)).collect()
 }
 
 #[cfg(test)]
