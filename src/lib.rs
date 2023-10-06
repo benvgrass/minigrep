21a3df38fb046d4aed9020b30550b6b2265d5e33
Ben Grass <bvgrass@outlook.com>
2023-10-05 21:25:23 -0400

    fixed iterator approach

--- src/lib.rs
+++ src/lib.rs
@@ -42,12 +42,12 @@ pub fn run(config: QueryConfig) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents.lines().filter(|line| line.contains(query)).collect()
+    contents.lines().filter(|line| line.contains(&query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    contents.lines().filter(|line| line.to_lowercase().contains(query)).collect()
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::{env, process};
 use minigrep::QueryConfig;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = QueryConfig::build(&args).unwrap_or_else(|err| {
+    let config = QueryConfig::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Issue parsing arguments: {err}");
         process::exit(1);
     });
